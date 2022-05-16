6465096b97010f890d47679f885742ca5d9e2b01
Drazor153 <fabianjusto2341@gmail.com>
2022-05-16 17:36:03 -0400

    paginas iniciales
    Primeras paginas?

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MarketFreak</title>
 </head>
 <body>
-    
+    <div>
+        <a href="catalogo.php">Catálogo</a>|
+        <a href="soporte.php">Soporte</a>|
+        <a href="login.php">Iniciar sesión</a>|
+        <a href="register.php">Registrarse</a>
+    </div>
 </body>
 </html>
