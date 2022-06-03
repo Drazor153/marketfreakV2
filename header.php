2929fe53446d6f8368ccf6fb96b4e8dd0c547bca
Drazor153 <fabianjusto2341@gmail.com>
2022-06-03 12:50:53 -0400

    a

--- header.php
+++ header.php
@@ -16,7 +16,12 @@
     <ul class="nav">
         <li><a href="catalogo.php">Catálogo</a></li>
         <li><a href="soporte.php">Soporte</a></li>
+        <li style="float:right"><a href="">Nombre Admin</a></li>
+        <li style="float:right"><a href="carrito.php">Carrito</a></li>
+        <li style="float:right"><a href="">Nombre Usuario</a></li>
+
         <li style="float:right"><a href="register.php">Registrarse</a></li>
         <li style="float:right"><a href="login.php">Iniciar sesión</a></li>
+        
     </ul>
 </header>
